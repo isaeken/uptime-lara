3911a37777f55850860b5d3f45c27394baee78f3
Isa Eken <hello@isaeken.com.tr>
2021-10-08 23:20:59 +0300

    update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        if (app()->environment() !== 'production') {
+            $this->call(UserSeeder::class);
+            $this->call(MonitorSeeder::class);
+        }
     }
 }
