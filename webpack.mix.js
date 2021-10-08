22e616d994f90067a1193b5ec54efe7eb2eb5b48
Isa Eken <hello@isaeken.com.tr>
2021-10-08 22:47:20 +0300

    add Laravel Jetstream

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
+mix
+    .setPublicPath('public_html')
+    .js('resources/js/app.js', 'js')
+    .postCss('resources/css/app.css', 'css', [
+        require('postcss-import'),
+        require('tailwindcss'),
     ]);
+
+if (mix.inProduction()) {
+    mix.version();
+}
