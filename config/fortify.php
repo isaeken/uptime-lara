0c85aac28fcf656bffc9cb1f99bd959651660312
Isa Eken <hello@isaeken.com.tr>
2021-10-09 00:41:53 +0300

    wip

--- config/fortify.php
+++ config/fortify.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Providers\RouteServiceProvider;
-use Laravel\Fortify\Features;
 
 return [
 
@@ -132,14 +131,14 @@ return [
     */
 
     'features' => [
-        Features::registration(),
-        Features::resetPasswords(),
+        // Features::registration(),
+        // Features::resetPasswords(),
         // Features::emailVerification(),
-        Features::updateProfileInformation(),
-        Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirmPassword' => true,
-        ]),
+        // Features::updateProfileInformation(),
+        // Features::updatePasswords(),
+        // Features::twoFactorAuthentication([
+        //    'confirmPassword' => true,
+        // ]),
     ],
 
 ];
